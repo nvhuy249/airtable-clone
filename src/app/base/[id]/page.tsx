48e86c9319619e75a65631b2f3eef04ad75a269b
nvhuy249 <109942049+nvhuy249@users.noreply.github.com>
2025-12-06 03:53:02 +1030

    bugs fixed for deployment

--- src/app/base/[id]/page.tsx
+++ src/app/base/[id]/page.tsx
@@ -3,11 +3,17 @@ import { redirect } from "next/navigation";
 import { serverCaller } from "~/trpc/server";
 import BaseClient from "./BaseClient";
 
-export default async function BasePage({ params }: { params: { id: string } }) {
+export default async function BasePage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
   const session = await auth();
   if (!session) redirect("/login");
 
-  const base = await serverCaller.base.byId({ id: params.id });
+  const base = await serverCaller.base.byId({ id });
 
   if (!base) redirect("/");
 
