8b885da9b89482edb16070e32de9d7b2c8e5bc4d
nvhuy249 <109942049+nvhuy249@users.noreply.github.com>
2025-12-13 00:30:36 +1030

    ok

--- src/app/base/[id]/page.tsx
+++ src/app/base/[id]/page.tsx
@@ -1,21 +1,23 @@
 import { auth } from "~/server/auth";
-import { redirect } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
 import { serverCaller } from "~/trpc/server";
 import BaseClient from "./BaseClient";
 
 export default async function BasePage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
 
   const session = await auth();
   if (!session) redirect("/login");
 
   const base = await serverCaller.base.byId({ id });
 
-  if (!base) redirect("/");
+  if (!base) {
+    notFound()
+  };
 
   return (
     <BaseClient
